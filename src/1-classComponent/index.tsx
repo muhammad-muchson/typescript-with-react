e5e7df60d93de621d8fa6878e95aa29b0d3eb0e1
attoyibi <muchson@alterra.id>
2021-11-02 09:35:26 +0700

    interface props

---
+++ src/1-classComponent/index.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+interface State {
+  counter: number;
+}
+interface Props {
+  title: string;
+  nama?: string;
+}
+export class ClassComp extends Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+    // this._addCount = this._addCount.bind(this);
+  }
+  _addCount(param: number): void {
+    // this.setState({ counter: this.state.counter + 1 });
+    this.setState({ counter: param + 1 });
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        {this.state.counter}
+        {this.props.nama}
+        <div>
+          {/* <button onClick={this._addCount.bind(this)}>Add Count</button> */}
+          {/* <button onClick={() => this._addCount()}>Add Count</button> */}
+          <button onClick={this._addCount.bind(this, this.state.counter)}>
+            Add Count
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ClassComp;
